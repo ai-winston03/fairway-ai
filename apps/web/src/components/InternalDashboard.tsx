46dadf9c5cd1f3542244071c49d105749e0c5be4
lgnrsprs-max <lgnrsprs@icloud.com>
2026-08-26 22:38:34 -0500

    Restore InternalDashboard exports so Golf Overview compiles

--- apps/web/src/components/InternalDashboard.tsx
+++ apps/web/src/components/InternalDashboard.tsx
@@ -1,1 +1,8 @@
-PLACEHOLDER_USE_FILE
+import { CalendarDays, ChartNoAxesCombined, CircleGauge, ClipboardList, Cloud, Flag, MessageSquareText, ReceiptText, RefreshCw, Users } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { ClubSettingsPanel } from "@/components/ClubSettingsPanel";
+import { MemberWorkspace } from "@/components/MemberWorkspace";
+import { StaffHoldsQueue } from "@/components/StaffHoldsQueue";
+import { evaluateWorkflowSafety, workflowLibrary } from "@/lib/workflows";
+
+export type OperationsArea = "golf" | "pro-shop" | "clubhouse" | "members" | "automations" | "platform";
